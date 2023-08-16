180538081cd37436c5c5f150184fd90a6f97ee87
Dj Amie X <32149397+djamiex@users.noreply.github.com>
2023-08-16 09:17:39 +0800

    Update my.lua

--- qb-crypto/locales/my.lua
+++ qb-crypto/locales/my.lua
@@ -1,23 +1,23 @@
 local Translations = {
     error = {
-        you_dont_have_a_cryptostick = 'You don\'t have a cryptostick',
-        cryptostick_malfunctioned = 'Cryptostick malfunctioned'
+        you_dont_have_a_cryptostick = 'Anda tidak mempunyai cryptostick',
+        cryptostick_malfunctioned = 'Cryptostick tidak berfungsi'
     },
     success = {
-        you_have_exchanged_your_cryptostick_for = 'You have exchanged your Cryptostick for: %{amount} QBit(s)'
+        you_have_exchanged_your_cryptostick_for = 'Penukaran Cryptostick berjumlah: %{amount} QBit(s)'
     },
     credit = {
-        there_are_amount_credited = 'You have been credited %{amount} Qbit(s)!',
-        you_have_qbit_purchased = 'You have purchased %{dataCoins} Qbit(s)!'
+        there_are_amount_credited = 'Jumlah telah dikreditkan %{amount} Qbit(s)!',
+        you_have_qbit_purchased = 'Anda telah membeli %{dataCoins} Qbit(s)!'
     },
     debit = {
-        you_have_sold = 'You have sold %{dataCoins} Qbit(s)!'
+        you_have_sold = 'Jumlah telah dijual %{dataCoins} Qbit(s)!'
     },
     text = {
-        enter_usb = '[E] - Enter USB',
-        system_is_rebooting = 'System is rebooting - %{rebootInfoPercentage} %',
+        enter_usb = '[E] - Masuk USB',
+        system_is_rebooting = 'Sistem sedang diproses - %{rebootInfoPercentage} %',
         you_have_not_given_a_new_value = 'You have not given a new value ... Current value: %{crypto}',
-        this_crypto_does_not_exist = 'This crypto does not exist, available crypto(s): Qbit',
+        this_crypto_does_not_exist = 'Crypto ini tidak wujud, jumlah crypto(s): Qbit',
         you_have_not_provided_crypto_available_qbit = 'You have not provided Crypto, available: Qbit',
         the_qbit_has_a_value_of = 'Qbit has a value of: %{crypto}',
         you_have_with_a_value_of = 'You have %{playerPlayerDataMoneyCrypto} QBit(s) with a value of: %{mypocket},-'
